efe565bcd12addd1d331c4e333c5e8c35a783982
BAjchler <b.ajchler@gmail.com>
2022-08-11 01:28:42 +0200

    Added documentation to SyncAlgorithms class.

--- Server/sync_algorithm.py
+++ Server/sync_algorithm.py
@@ -2,11 +2,32 @@ ACCEPTABLE_DESYNC = 2000
 
 
 class SyncAlgorithms:
+    """
+    Class containing methods for playback synchronization.
+    """
 
     @staticmethod
-    def sync_playback_time(priority_user: int, host_delay: int, user_delay: int) -> int:
-        return priority_user + (host_delay // 2) + (user_delay // 2)
+    def sync_playback_time(host_time: int, host_delay: int, user_delay: int) -> int:
+        """
+        Generates new correct time created from current host playback time.
+
+        :param host_time: host playback time.
+        :param host_delay: host->server->host delay in milliseconds.
+        :param user_delay: user->server->user delay in milliseconds.
+        :return: new playback time value close to host playback time.
+        """
+        return host_time + (host_delay // 2) + (user_delay // 2)
+
 
     @staticmethod
     def determine_sync(host_time: int, host_delay: int, user_time: int, user_delay: int) -> bool:
+        """
+        Determines whether synchronization between host and another user is needed.
+
+        :param host_time: host playback time.
+        :param user_time: user playback time.
+        :param host_delay: host->server->host delay in milliseconds.
+        :param user_delay: user->server->user delay in milliseconds.
+        :return: boolean value, true if synchronization is needed.
+        """
         return (abs(host_time-user_time) - host_delay - user_delay) > ACCEPTABLE_DESYNC
--- Test/test_sync_algorithm.py
+++ Test/test_sync_algorithm.py
@@ -4,12 +4,17 @@ from Server.sync_algorithm import SyncAlgorithms
 
 
 class TestSyncAlgorithms(TestCase):
+    """
+    Class with synchronization algorithm tests.
+
+    """
+
     def test_determine_sync(self):
         host_time = 4000
         member_time = 1100
         host_delay = 20
         member_delay = 30
-
+        """Test if determining synchronization is correct"""
         self.assertEqual(SyncAlgorithms.determine_sync(host_time, host_delay, member_time, member_delay), True)
         self.assertEqual(SyncAlgorithms.determine_sync(host_time, host_delay, host_time, host_delay), False)
 
@@ -17,6 +22,6 @@ class TestSyncAlgorithms(TestCase):
         host_time = 4000
         host_delay = 20
         member_delay = 30
-
+        """Test if playback is changed to host + delay time."""
         self.assertEqual(SyncAlgorithms.sync_playback_time(host_time, host_delay, member_delay), 4025)
 
